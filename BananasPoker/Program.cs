a3cfa45b2466f8e4388308b65dbd9b1f0268caa1
bstauff <brian.stauff@gmail.com>
2018-01-16 15:57:22 -0600

    Banans poker to poke some grains

---
+++ BananasPoker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Runtime.Configuration;
+
+namespace BananasPoker
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            ClientConfiguration clientConfig = ClientConfiguration.LocalhostSilo();
+
+            IClusterClient client = new ClientBuilder().UseConfiguration(clientConfig).Build();
+
+            await client.Connect();
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
